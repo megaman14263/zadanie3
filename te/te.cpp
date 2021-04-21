2e863ae86bdaf846c027a4b81450c01bf3e9639d
megaman14263 <82954582+megaman14263@users.noreply.github.com>
2021-04-21 17:07:00 +0300

    Add file

---
+++ te/te.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Инициализируем переменные
+    int K, sum = 0;
+    int a, b, c;
+    cin >> K;
+    a = 1;
+    b = 1;
+    while (K > 0)
+    {
+        K--;
+        c = a + b;
+        cout << a << " ";
+        sum += a;
+        a = b;
+        b = c;
+    }
+    cout << "\n" << sum << "\n";
+    return 0;
+    cin.get();
+}
